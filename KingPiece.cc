f414a5b5e88e3537bcbcfab5cad7aedf86494477
Arnav Srivastava <arnavsri20@gmail.com>
2025-03-13 17:24:24 -0400

    Part 3 Done

--- KingPiece.cc
+++ KingPiece.cc
@@ -21,7 +21,7 @@ bool KingPiece::canMoveToLocation(int toRow, int toCol)
 
     // The interesting thing about moving the king is that he can move to any spot adjacent to him
     // we already check boundary condition in isValidMove (so no need to do that here too)
-    if (abs(toRow - currRow) == 1 || abs(toCol - currCol) == 1)
+    if (abs(toRow - currRow) <= 1 || abs(toCol - currCol) <= 1)
     {
         // now we gotta implement a feature that prevents the king from moving to a square if it is dangerous BUT
         // we also need to ensure that movePiece doesn't do anything if moving the king is dangerous
